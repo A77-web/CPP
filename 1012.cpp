4abbbc4903880452a42469133c491b4a0bdeaa8c
A77-web <83652921+A77-web@users.noreply.github.com>
2021-10-12 20:52:30 +0800

    拷贝构造函数

---
+++ 1012.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+class A {
+private:
+	int a;
+public:
+	A(int b) :a(b) {
+		cout << "构造函数" << endl;
+	}
+	A(const A& c) {
+		a = c.a;
+		cout << "拷贝构造函数" << endl;
+	}
+	~A() {
+		cout << "析构函数" << endl;
+
+	}
+};
+int main() {
+	A a(100);
+	A b = a;
+	return 0;
+
+}
