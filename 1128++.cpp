be9ffc873fff7e8c89d75425ebddc0473d1baff3
A77-web <83652921+A77-web@users.noreply.github.com>
2021-11-28 14:21:02 +0800

    Add files via upload

---
+++ 1128++.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution
+{
+public:
+	int StrToInt(string str)
+	{
+		if (str.size() == 0)
+			return 0;
+		int i = 0;
+		int sign = 1;//ÕýÊý
+		if (str[0] == '+' || str[0] == '-')
+		{
+			if (str[0] == '-')
+				sign = -1;
+			i++;
+		}
+		int num = 0;
+		while (i < str.size())
+		{
+			if (str[i] < '0' || str[i]>'9')
+				return 0;
+			num = num * 10 + str[i] - '0';
+			i++;
+		}
+		return sign * num;
+
+	}
+};
+void main()
+{
+	Solution a;
+	cout<<a.StrToInt("-2131241");
+}
