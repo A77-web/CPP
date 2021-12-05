48c92869591a1f5df6cc779b41db0302c34a47b9
A77-web <83652921+A77-web@users.noreply.github.com>
2021-12-05 16:08:29 +0800

    用两个队列实现栈 stackOJ

--- 1205.cpp
+++ 1205.cpp
@@ -1,9 +1,51 @@
-class Solution {
+#include<iostream>
+#include<queue>
+using namespace std;
+class MyStack {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        int n=size(nums);
-        sort(nums.begin(),nums.end());
-        return nums[n-k];
+	queue<int>queue1;
+	queue<int>queue2;
+	MyStack() {
 
-    }
+	}
+	void push(int x)
+	{
+		queue2.push(2);
+		while (!queue1.empty())
+		{
+			queue2.push(queue1.front());
+			queue1.pop();
+		}
+		swap(queue1, queue2);
+	}
+	int pop()
+	{
+		int r = queue1.front();
+		queue1.pop();
+		return r;
+	}
+	int top()
+	{
+		int r = queue1.front();
+		return r;
+	}
+	bool empty()
+	{
+		return queue1.empty();
+	}
 };
+void main()
+{	
+	MyStack mystack;
+	queue<int>q ;
+	vector<int>v = { 1,2,4 };
+	for (const auto& e : v)
+		q.push(e);
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+	mystack.push(2);
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+}
