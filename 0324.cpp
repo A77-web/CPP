3657093b3a191f5b96bfebbcef924380c5ea4d36
A77-web <83652921+A77-web@users.noreply.github.com>
2022-03-24 16:58:36 +0800

    数根oj+

--- 0324.cpp
+++ 0324.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
-    double x,y,z;
-    while(cin>>x>>y>>z)
-    {
-        if(x+y>z&&x+z>y&&y+z>x)
-        {
-            cout<<"Yes"<<endl;
-        }
-        else{
-        cout<<"No"<<endl;
-    }
-    }
-    return 0;
+	
+	string str;
+	//用字符串接收，防止数字过大
+	while (cin >> str)
+	{
+		int num = 0;
+		for (int i = 0; i < str.size(); i++)
+		{
+			num += str[i] -'0';
+		}
+		int sum = 0;
+		while (num)
+		{
+			sum += num % 10;
+			num = num / 10;
+			if (num == 0 && sum / 10 != 0)
+			{
+				num=sum  ;
+				sum = 0;
+			}
+		}
+		cout << sum << endl;
+	}
 }
