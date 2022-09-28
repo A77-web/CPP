2c2f65c5c99adbf2ef9b956461efe9ae750df220
A77-web <83652921+A77-web@users.noreply.github.com>
2022-09-28 20:31:22 +0800

    只出现一次的数字2

---
+++ 0928.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            int total = 0;
+            for (int num: nums) {
+                total += ((num >> i) & 1);
+            }
+            if (total % 3) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+};
+
